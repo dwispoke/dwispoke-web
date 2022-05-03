bc78e3159c5904b946441c1397a73930ff203c37
yuda1124 <yuda1124@naver.com>
2022-05-03 23:18:43 +0900

    Top 버튼 UI, 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React, { useRef } from "react";
 import { Header, ProductInfo } from "./components";
+import { useScroll } from "./hooks";
+import style from "./App.module.scss";
 
 function App() {
+  const appRef = useRef(null);
+  const { scrollTop } = useScroll(appRef);
+  const scrollToTop = () => {
+    appRef.current.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
   return (
-    <div className="App">
+    <div className={style.App} ref={appRef}>
       <Header />
       <ProductInfo />
+      {scrollTop > 0 && (
+        <button className={style.btnUp} onClick={scrollToTop} />
+      )}
     </div>
   );
 }
