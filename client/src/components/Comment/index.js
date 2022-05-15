9e3a168abe0c671a345b83be883723120d3dae50
yuda1124 <yuda1124@naver.com>
2022-05-15 21:39:32 +0900

    좋아요 UI 구현

--- client/src/components/Comment/index.js
+++ client/src/components/Comment/index.js
@@ -2,12 +2,22 @@ import React from "react";
 import style from "./index.module.scss";
 import { StarBox } from "..";
 
-const Comment = ({ text, score, nickname }) => {
+const Comment = ({ text, score, nickname, liked }) => {
   return (
     <li className={style.container}>
       <StarBox score={score} />
       <p className={style.text}>{text}</p>
       <div className={style.userinfo}>
+        <div className={style.wrap_like}>
+          <p>상품평이 도움이 됐다면 꾹!</p>
+          <button>
+            <img
+              src="https://www.samsung.com/sec/static/_images/common/icon-goods.svg"
+              alt="좋아요"
+            />
+            <span>{liked}</span>
+          </button>
+        </div>
         <span>{nickname}</span>
       </div>
     </li>
